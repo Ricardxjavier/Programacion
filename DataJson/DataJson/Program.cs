0cb14add83de78a49872307190a8685489464822
Ricardxjavier <159715620+Ricardxjavier@users.noreply.github.com>
2024-12-03 20:24:28 -0400

    Add files via upload

---
+++ DataJson/DataJson/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace JsonEjemplo
+{
+    public class Mascota
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public Type Type { get; set; }
+    }
+    public enum Type
+    {
+        Gato, Perro, Ave, Otros
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            var Mascota = new Mascota()
+            {
+                Nombre = " Ricardo perro",
+                Edad = 7,
+                Type = Type.Perro
+
+
+
+            };
+            var optios = new JsonSerializerOptions { WriteIndented = true };
+            string Jsonstring = JsonSerializer.Serialize(Mascota, optios);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(Jsonstring);
+            Console.ReadKey();
+
+        }
+
+    }
+
+
+}
