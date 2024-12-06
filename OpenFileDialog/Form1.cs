41de63dcb7c21e22f04d2f181e034cbf7ad024b0
Ricardxjavier <159715620+Ricardxjavier@users.noreply.github.com>
2024-12-06 14:19:03 -0400

    Add files via upload

---
+++ OpenFileDialog/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenFileDialog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK) 
+            {
+
+                lblAbrir.Text = openFileDialog1.FileName;
+            }
+
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK) 
+            { 
+                lblSalvar.Text = saveFileDialog1.FileName;
+            }
+        }
+    }
+}
